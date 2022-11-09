3deef8e4c217d12207dfe0def23e9aa99e414ba8
Mikkel Øer Lund <mikkel00lundna1@gmail.com>
2022-11-09 20:12:32 +0100

    CreationalPatternsExamples
    Added creational patterns example. And creational patterns structures.

--- Program.cs
+++ Program.cs
@@ -1,13 +1,14 @@
 ﻿
-
-
+using CreationalPatterns.Examples;
 
 namespace DesignPatterns
 {
+ 
     internal class Program
     {
         public static void Main()
         {
+            new AbstractFactoryExample().Run();
         }
     }
 }
